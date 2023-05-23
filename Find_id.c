b9d1cb1efb3359c3af68b736e75c2f00cc12f1fd
저우 가오펑 <62272144+James6945@users.noreply.github.com>
2023-05-23 11:23:51 +0900

    Find_id.c

---
+++ Find_id.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+static int count_ID=0;
+
+void find_ID()
+{
+    count_ID++;
+    count_service++;
+    printf("\n You visiting the %s game.\n",title);
+    
+    printf("\n Total number o frequests:%d,the number of requests for ID_sarch:%d.\n",count_service, count_ID);
+    
+}
